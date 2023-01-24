f21c20db7b0f4fb308eb17dc524e3a86c4c04628
wisnieskii <106274579+wisnieskii@users.noreply.github.com>
2023-01-24 09:35:25 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
+import powerImg from "./assets/powered.png";
+import leftArrowImg from "./assets/leftarrow.png";
+import { GridItem } from "./components/Griditem";
 
-function App() {
+import { levels, calculateImc, Level } from "./helpers/imc";
+
+export const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
+
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField));
+    } else {
+      alert("Preencha todos os campos!");
+    }
+  };
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={powerImg} alt="" width={150} />
+        </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC</h1>
+          <p>
+            IMC é a sigla para Indíce de Massa Corpórea, parâmetro adotado pela
+            Organização Mundial de Saúde para calcular o pesso ideal de cada
+            pessoa
+          </p>
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex:1.5 (em metros)"
+            value={heightField > 0 ? heightField : ""}
+            onChange={(e) => setHeightField(parseFloat(e.target.value))}
+          />
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex:75.3 (em kg)"
+            value={weightField > 0 ? weightField : ""}
+            onChange={(e) => setWeightField(parseFloat(e.target.value))}
+          />
+          <button onClick={handleCalculateButton}>Calcular</button>
+        </div>
+        <div className={styles.rightSide}>
+          {!toShow && (
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} item={item}></GridItem>
+              ))}
+            </div>
+          )}
+          {toShow && (
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImg} alt="" width={25} />
+              </div>
+              <GridItem item={toShow} />
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/components/Griditem/index.tsx
@@ -0,0 +1,32 @@
+import { Level } from "../../helpers/imc";
+import styles from "./Griditem.module.css";
+import upImg from "../../assets/up.png";
+import downImg from "../../assets/down.png";
+
+
+type Props = {
+  item: Level;
+};
+
+export const GridItem = ({ item }: Props) => {
+  return (
+    <div className={styles.main} style={{ backgroundColor: item.color }}>
+      <div className={styles.gridIcon}>
+        {item.icon === "up" && <img src={upImg} alt="" width="30" />}
+        {item.icon === "down" && <img src={downImg} alt="" width="30" />}
+      </div>
+      <div className={styles.gridTitle}>{item.title}</div>
+
+      {item.yourImc && (
+        <div className={styles.yourImc}>Seu IMC é de {item.yourImc} kg/m²</div>
+      )}
+
+      <div className={styles.gridInfo}>
+        <>
+          IMC está entre <strong>{item.imc[0]}</strong> e{" "}
+          <strong>{item.imc[1]}</strong>
+        </>
+      </div>
+    </div>
+  );
+};
